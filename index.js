4371da2817592f05575075ce06d2ed413160ed86
leandropharmativa <leandro@pharmativa.com.br>
2025-04-23 11:24:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -62,4 +62,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+app.get('/', (req, res) => {
+  res.json({ mensagem: "API Controle de Dívidas online" });
+});
 
